8ac9a707837f2d1eff54289c82e455eb0a9b1f12
LePunkd <justin_antu@hotmail.com>
2023-11-20 12:40:18 +1100

    Initial Commit

---
+++ myPTV/myPTVApp.swift
@@ -0,0 +1,17 @@
+//
+//  myPTVApp.swift
+//  myPTV
+//
+//  Created by Justin Antu on 20/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct myPTVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
